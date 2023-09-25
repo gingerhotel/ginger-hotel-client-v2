ee1530143db205e30c0dffc8cb447ddc110c27d1
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-09-25 10:58:15 +0100

    feat: 폰트 적용

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -7,7 +7,12 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Text style={styles.text}>안녕하세용가리?</Text>
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
@@ -16,16 +21,19 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  text: {
+    fontFamily: "Pretendard-Regular",
   },
 });
