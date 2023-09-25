36e9943d4a414e5d28035ab4571f5ac004d9b762
2ujin <suddenlydisappear200@gmail.com>
2023-09-25 15:40:16 +0100

    feat: 버튼 옮겨오기, 스크롤뷰 구현
    svg 이미지 오류 해결 ㅠㅠ

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,20 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { Image, Platform, ScrollView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import Header from "../../components/Header";
+import { MonoText } from "../../components/StyledText";
+import React from "react";
+import { WithLocalSvg } from "react-native-svg";
+import Buttons from "../../components/Buttons/buttons";
+const SVG = require("../../assets/images/StartHotel.svg");
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen({ navigation }: any) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Text style={styles.text}>안녕하세용가리?</Text>
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <ScrollView>
+      <Header />
+      <View style={styles.container}>
+        <MonoText style={styles.hotel_name}>진저님의 진저호텔</MonoText>
+        <MonoText style={styles.hotel_desc}>
+          진저의 호텔에 오신 여러분 환영합니다~!
+        </MonoText>
+        {Platform.OS === "ios" || Platform.OS === "android" ? (
+          <WithLocalSvg width={280} asset={SVG} />
+        ) : (
+          <Image source={SVG} style={styles.hotel_img} />
+        )}
+        <Buttons
+          navigation={navigation}
+          url={"login"}
+          title="호텔 편지함"
+          color="red"
+        />
+        <Buttons
+          navigation={navigation}
+          url={"login"}
+          title="진저맨 앨범"
+          color="green"
+        />
+        <Buttons
+          navigation={navigation}
+          url={"login"}
+          title="호텔 링크 복사하기"
+          color="white"
+        />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -22,7 +50,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+  },
+  hotel_img: {
+    width: 300,
+    height: 400,
+    marginTop: 20,
   },
   title: {
     fontSize: 20,
@@ -35,5 +68,13 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: "Pretendard-Regular",
+  },
+  hotel_name: {
+    marginTop: 35,
+    fontSize: 25,
+  },
+  hotel_desc: {
+    fontSize: 15,
+    marginTop: 10,
   },
 });
