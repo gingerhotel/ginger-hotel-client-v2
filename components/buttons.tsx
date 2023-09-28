e70334f4abd6715355ebb3a7da8bc5585a15533d
2ujin <suddenlydisappear200@gmail.com>
2023-09-28 17:33:34 +0100

    feat: 임시 버튼 수정
    ios에는 왜 border-radius가 안 먹히는가

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -14,44 +14,31 @@ type Props = {
 
 const Buttons = ({ title, color, is_disable, navigation, url }: Props) => {
   return (
-    <View style={cstyles(COLORS[color], color).cover}>
-      <Link
-        href={`/${url}`}
-        style={[styles.button, cstyles(COLORS[color], color).background]}
-        disabled={is_disable}
-      >
-        <MonoText style={cstyles(COLORS[color], color).text}>{title}</MonoText>
-      </Link>
-    </View>
+    // <View >
+    <Link
+      href={`/${url}`}
+      disabled={is_disable}
+      style={cstyles(COLORS[color], color).button}
+    >
+      <MonoText style={cstyles(COLORS[color], color).text}>{title}</MonoText>
+    </Link>
+    // </View>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    width: 281,
-    height: 29,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderStyle: "dashed",
-    borderRadius: 2,
-    borderWidth: 1.5,
-    textAlign: "center",
-  },
-});
-
 const cstyles = (color_code: string, color: string) =>
   StyleSheet.create({
-    background: {
-      borderColor: color === "white" ? COLORS.red : "white",
-      backgroundColor: color_code,
-    },
-    cover: {
-      backgroundColor: color_code,
-      padding: 4,
-      borderRadius: 5,
+    button: {
+      width: 281,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      padding: 11,
+      borderRadius: 10,
       marginTop: 14,
       marginBottom: 14,
+      backgroundColor: color_code,
       borderWidth: color === "white" ? 1.5 : 0,
       borderColor: color === "white" ? COLORS.red : "none",
     },
