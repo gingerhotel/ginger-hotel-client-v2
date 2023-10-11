f678075d3352ddbef2179610a1421a52036330d0
minsuRob <minsu.rob@gmail.com>
2023-10-11 12:13:19 +0900

    fix: graphql 내용 삭제
    rest api 처리를 위한 변경

--- metro.config.js
+++ metro.config.js
@@ -1,25 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
 /** @type {import('expo/metro-config').MetroConfig} */
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname,{
-    isCSSEnabled: true,
-  });
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-      //babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json', 'mjs'] //add here
-    },
-  };
-})();
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
 
+module.exports = config;
