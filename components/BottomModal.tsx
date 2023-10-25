59ca5ecbe83ce11e9eeaf8460db2690c3135bdac
minsuRob <minsu.rob@gmail.com>
2023-10-25 18:51:11 +0900

    fix: metro exist 오류로 인한 대소문자 문제 수정
    신규환경에서 계속 발생시 파일명을 소문자로 변경해야함. 일괄로 변경이 가능할지 확인 필요.

--- components/BottomModal.tsx
+++ components/BottomModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { BottomSheet } from "react-native-btr";
-import CenterModal from "./\bcenterModal";
+import CenterModal from "./centerModal";
 import { MonoText } from "./styledText";
 
 const BottomModal = ({
