8a19e4fc73546b3e03514779c5d6a5f860ad14b6
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-10-26 16:39:40 +0100

    feat: 폰트 변경 및 상수 색상 추가

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,10 +1,6 @@
+import { typography } from "../constants/Typo";
 import { Text, TextProps } from "./themed";
 
 export function MonoText(props: TextProps) {
-  return (
-    <Text
-      {...props}
-      style={[props.style, { fontFamily: "Pretendard-Regular" }]}
-    />
-  );
+  return <Text {...props} style={[props.style, typography.label]} />;
 }
---
+++ constants/Typo.ts
@@ -0,0 +1,9 @@
+import { colors } from "./Colors";
+import "typeface-nanum-square-neo";
+
+export const typography = {
+  label: {
+    fontFamily: "NanumSquareNeo-Variable",
+    color: colors.Black,
+  },
+};
