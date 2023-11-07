45e89e9afd084dac436d50e0290e1862bea91e1e
2ujin <suddenlydisappear200@gmail.com>
2023-11-07 11:28:55 +0000

    feat: 답장보내기 input 작업
    border에 있는 사선 줄무늬는 일단 보류 ...

--- app/letter.tsx
+++ app/letter.tsx
@@ -1,78 +1,63 @@
-import React from "react";
-import {
-  StyleSheet,
-  Image,
-  Button,
-  ScrollView,
-  View,
-  TextInput,
-} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
 import Buttons from "../components/buttons";
-import LetterHeader from "../components/letterHeader";
+import LetterInput from "../components/letterInput";
+import { MonoText } from "../components/styledText";
 import { colors } from "../constants/Colors";
 
-function Letter({ navigation }: any) {
+export default function Letter({ navigation }: any) {
+  const [letterText, setLetterText] = useState("");
+
+  const handleInputChange = (text: string) => {
+    setLetterText(text);
+    console.log(letterText);
+  };
+
   return (
     <View style={styles.container}>
-      {/* <LetterHeader marginTop={100} isTitle={false} /> */}
-      <ScrollView>
-        <View style={styles.mailbox_items}>
-          <TextInput
-            style={styles.letter}
-            multiline={true}
-            numberOfLines={20}
-            placeholder="전하고 싶은 말을 적어주세요!"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="닉네임을 입력하세요 (10자 이하)"
-          />
-          <Buttons url={"gingercard"} title="이미지 첨부" color="darkgray" />
-          <Buttons
-            navigation={navigation}
-            url={"completed"}
-            title="보내기"
-            color="green"
-          />
-        </View>
-      </ScrollView>
+      <View style={styles.mailbox_items}>
+        <MonoText style={{ color: colors.Whiteyello, marginBottom: 12 }}>
+          따뜻한 편지를 보내 준 친구에게 마음을 전해요{" "}
+        </MonoText>
+
+        <LetterInput value={letterText} onChange={handleInputChange} />
+        <TextInput
+          style={styles.input}
+          placeholder="닉네임을 입력하세요 (10자 이하)"
+        />
+        <Buttons url={"gingercard"} title="이미지 첨부" color="green" />
+        <Buttons
+          navigation={navigation}
+          url={"completed"}
+          title="보내기"
+          color="green"
+        />
+      </View>
     </View>
   );
 }
-
-Letter.navigationOptions = {
-  title: "타이틀1",
-};
-
-export default Letter;
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: colors.greyblack,
     height: "100%",
   },
   mailbox_items: {
     backgroundColor: colors.greyblack,
     padding: 15,
     display: "flex",
+    justifyContent: "center",
     alignItems: "center",
     width: "100%",
     height: "100%",
-  },
-  letter: {
-    width: 300,
-    backgroundColor: "white",
-    padding: 10,
-    textAlign: "center",
   },
   input: {
     width: 300,
     backgroundColor: "white",
     padding: 10,
     marginTop: 20,
-    textAlign: "center",
+    textAlign: "left",
     marginBottom: 20,
   },
 });
