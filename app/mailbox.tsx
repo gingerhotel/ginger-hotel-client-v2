a9830518b9c853c92edd4599afe63969ac8a8ad6
minsuRob <91197026+c0c0pang@users.noreply.github.com>
2023-11-16 03:24:45 +0900

    feat: 새로운 편지와 답장 편집에 대한 조건부 렌더링

--- app/mailbox.tsx
+++ app/mailbox.tsx
@@ -1,46 +1,21 @@
 import React from "react";
 import { StyleSheet, Image, Button, ScrollView, View } from "react-native";
 import Buttons from "../components/buttons";
-import LetterItem from "../components/letterItem";
+import NewLetterItem from "../components/newLetterItem";
 import MailHeader from "../components/mailHeader";
+import ReplyLetterItem from "../components/replyLetterItem";
+import { useRecoilValue } from "recoil";
+import { letterSwitchState } from "../atom/letterAtom";
 
 export default function MailBox({ navigation }: any) {
+  const letterRender = useRecoilValue(letterSwitchState)
   return (
     <View style={styles.container}>
-
       <MailHeader marginTop={50} navigation={navigation} />
       <ScrollView>
         <View style={styles.mailbox_items}>
-          <LetterItem
-            navigation={navigation}
-            from={""}
-            contents={""}
-            is_active={false}
-          />
-          <LetterItem
-            navigation={navigation}
-            from={""}
-            contents={""}
-            is_active={false}
-          />
-          <LetterItem
-            navigation={navigation}
-            from={""}
-            contents={""}
-            is_active={false}
-          />
-          <LetterItem
-            navigation={navigation}
-            from={""}
-            contents={""}
-            is_active={false}
-          />
-          <LetterItem
-            navigation={navigation}
-            from={""}
-            contents={""}
-            is_active={false}
-          />
+          {letterRender.new ? (<NewLetterItem />) : (<ReplyLetterItem />)}
+
         </View>
       </ScrollView>
     </View>
