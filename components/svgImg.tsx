951b37a584cf15fe42b846f01fad2536a07893af
minsu.lee <minsu.rob@gmail.com>
2023-11-16 18:14:44 +0900

    fix: TODO:손봐야함 width, a_width

--- components/svgImg.tsx
+++ components/svgImg.tsx
@@ -8,8 +8,8 @@ type Props = {
   /* Todo : Need to size refactor */
   // a_width: number; // 앱 가로
   // a_height?: number; // 앱 세로
-  width?: number;
-  height?: number;
+  //width?: number;
+  //height?: number;
 };
 
 export function SvgImg({
@@ -17,18 +17,18 @@ export function SvgImg({
   url,
   // a_width,
   // a_height,
-  width,
-  height,
+  //width,
+  //height,
 }: Props) {
   return (
     <>
       {Platform.OS === "ios" || Platform.OS === "android" ? (
         <TouchableOpacity onPress={onPress}>
-          <WithLocalSvg asset={url} width={width} height={height} />
+          <WithLocalSvg asset={url} /*width={width} height={height}*/ />
         </TouchableOpacity>
       ) : (
         <TouchableOpacity onPress={onPress}>
-          <Image source={url} width={width} height={height} />
+          <Image source={url} /*width={width} height={height} *//>
         </TouchableOpacity>
       )}
     </>
