d0872792ab50c32c2faecb8c9efed71242cb0311
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-20 01:27:20 +0900

    feat: 바뀐 Id 변경

--- app/login.tsx
+++ app/login.tsx
@@ -15,51 +15,51 @@ const SVG = require("../assets/images/StartHotel.svg");
 
 export default function Login({ navigation }: any) {
   let clientId =
-    "983622613978-aj2js3e1qb9qq1jtdueu7ic47o8fgg2l.apps.googleusercontent.com";
+    "251638133705-q41nmhb0a21vrbj2vp5rmnn8n1bv2tjh.apps.googleusercontent.com";
 
   return (
-      <View style={styles.container}>
-        <Text style={styles.desc}>진저호텔에서 보내는 25일간의 휴일</Text>
-        <Text style={styles.title}>진저호텔</Text>
-        {Platform.OS === "ios" || Platform.OS === "android" ? (
-          <WithLocalSvg width={280} asset={SVG} />
-        ) : (
-          <Image source={SVG} style={styles.hotel_img} />
-        )}
+    <View style={styles.container}>
+      <Text style={styles.desc}>진저호텔에서 보내는 25일간의 휴일</Text>
+      <Text style={styles.title}>진저호텔</Text>
+      {Platform.OS === "ios" || Platform.OS === "android" ? (
+        <WithLocalSvg width={280} asset={SVG} />
+      ) : (
+        <Image source={SVG} style={styles.hotel_img} />
+      )}
 
-        <View style={styles.social_btn_group}>
-          <SocialButton name={"apple"} />
-          <SocialButton name={"google"} />
-          <SocialButton name={"kakao"} />
-          <SocialButton name={"naver"} />
-        </View>
-        <GoogleOAuthProvider clientId={clientId}>
-          <GoogleLogin
-            width={1000}
-            type={"icon"}
-            shape="circle"
-            onSuccess={async (credentialResponse) => {
-              const response = await axios.post(
+      <View style={styles.social_btn_group}>
+        <SocialButton name={"apple"} />
+        <SocialButton name={"google"} />
+        <SocialButton name={"kakao"} />
+        <SocialButton name={"naver"} />
+      </View>
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleLogin
+          width={1000}
+          type={"icon"}
+          shape="circle"
+          onSuccess={async (credentialResponse) => {
+            const response = await axios.post(
               'http://127.0.0.1:8080/auth/google',
               {
                 token: credentialResponse.credential,
               }
-              );
-              console.log(response);
-              //setUser({...userState, isLogin : true});
-              /* Need to RecoilRoot*/
-              AsyncStorage.setItem('isLogin', "true");
-              AsyncStorage.setItem('accessToken', response.data.accessToken);
-              navigation.push('hotelcreate')
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-          {/* // Todo: Google Custom Design link :
+            );
+            console.log(response);
+            //setUser({...userState, isLogin : true});
+            /* Need to RecoilRoot*/
+            AsyncStorage.setItem('isLogin', "true");
+            AsyncStorage.setItem('accessToken', response.data.accessToken);
+            navigation.push('hotelcreate')
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+        {/* // Todo: Google Custom Design link :
           https://velog.io/@miyoni/google-social-login */}
-        </GoogleOAuthProvider>
-      </View>
+      </GoogleOAuthProvider>
+    </View>
   );
 }
 
