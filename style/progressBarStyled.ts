e4b97363e05f364d3f6b225ba1d833e537643c42
seocylucky <chaeyeon2285@naver.com>
2023-11-19 22:02:34 +0900

    style: 프로그래스바 디자인 작업

--- style/progressBarStyled.ts
+++ style/progressBarStyled.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components/native";
 
 export const ProgressBarView = styled.View`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
-    gap: 20;
-    width: 60%;
-    padding: 10px;
-`
-export const Bar = styled.View` 
-    flex:1;
-    justify-content: center;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 6;
+  width: 60%;
+  margin-top: 25px;
+`;
+export const Bar = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
