8efe15f8c1b88f5ac2181a86bc330414b395a2d2
seocylucky <chaeyeon2285@naver.com>
2023-11-19 21:53:42 +0900

    feat: Quicksand 폰트 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
   const [fontsLoaded] = useFonts({
     "NanumSquareNeo-Variable": require("../assets/fonts/NanumSquareNeo-Variable.ttf"),
     "SOYOMaple-Regular": require("../assets/fonts/SOYO-Maple-Regular.ttf"),
+    "Quicksand-Variable": require("../assets/fonts/Quicksand-VariableFont_wght.ttf"),
   });
   if (!fontsLoaded) {
     return <></>;
