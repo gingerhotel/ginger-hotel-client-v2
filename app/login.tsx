2940a05566cdafd9510a88858b4d18e432623e66
minsu.lee <minsu.rob@gmail.com>
2023-11-22 12:18:07 +0900

    feat: test login 추가, input이 uid

--- app/login.tsx
+++ app/login.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { StyleSheet, Image, View, Platform, Text, Button, Alert } from "react-native";
+import { StyleSheet, Image, View, Platform, Text, Button, Alert, TextInput } from "react-native";
 // import { MonoText } from "../components/styledText";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GoogleLogin } from "@react-oauth/google";
@@ -12,6 +12,8 @@ import SocialButton from "../components/socialButton";
 import axios from 'axios';
 
 import * as AppleAuthentication from "expo-apple-authentication";
+import { ResponseType } from "expo-auth-session";
+import { FieldValues, useForm } from "react-hook-form";
 
 
 //import { useRecoilValue, RecoilRoot, useSetRecoilState } from "recoil";
@@ -26,63 +28,70 @@ export default function Login({ navigation }: any) {
   });
 
   const [token, setToken] = React.useState("");
+  const { setValue, register, control, handleSubmit } = useForm();
+
+  React.useEffect(() => {
+    register("socialId");
+  }, [register]);
 
   React.useEffect(() => {
     handleEffect();
-  }, [response, token]);
+  }, [response]);
+
+  const handleLoginTest = async (data: FieldValues) => {
+    axios
+      .post("http://localhost:8080/auth/test", {
+        socialId: data.socialId,
+        vendor: "NAVER",
+      })
+      .then((res) => {
+        console.log(res);
+        AsyncStorage.setItem('isLogin', "true");
+        AsyncStorage.setItem('accessToken', res.data.accessToken);
+        navigation.push('hotelcreate')
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  };
+
+
+  async function handleEffect() {
+    //const user = await getLocalUser();
+    //console.log("user", user);
+    //if (!user) {
+      if (response?.type === "success") {
+        // setToken(response.authentication.accessToken);
+       // getUserInfo(response.authentication.accessToken);
+       console.log("succ");
+      }
+    // } else {
+    //   setUserInfo(user);
+    //   console.log("loaded locally");
+    // }
+  }
+
+    const loginWithGoogle = () => {
+      //const a:string = response.authentication?.accessToken;
+    }
 
     // Google 로그인 처리하는 함수
-    const handleSignInWithGoogle = async () => {
-      const user = await AsyncStorage.getItem("@user");
+    /*const handleSignInWithGoogle = async () => {
+      const user = await AsyncStorage.getItem("accessToken");
       if (!user) {
         if (response?.type === "success") {
           // 인증 요청에 대한 응답이 성공이면, 토큰을 이용하여 유저 정보를 가져옴.
           // await getUserInfo(response.authentication?.accessToken);
+          AsyncStorage.setItem('isLogin', "true");
+          AsyncStorage.setItem('accessToken', response.data.accessToken);
+          navigation.push('hotelcreate')
         }
       } else {
         // 유저 정보가 이미 있으면, 유저 정보를 가져옴.
         setUserInfo(JSON.parse(user));
       }
-    };
-
-    const getUserInfo = async (token:string) => {
-      if (!token) return;
-      try {
-        const response = await fetch(
-          "https://www.googleapis.com/userinfo/v2/me",
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        );
-  
-        const user = await response.json();
-        await AsyncStorage.setItem("@user", JSON.stringify(user));
-        setUserInfo(user);
-      } catch (error) {
-        // Add your own error handler here
-      }
-    };
-  
-    async function handleEffect() {
-      const user = await getLocalUser();
-      console.log("user", user);
-      if (!user) {
-        //Alert.alert(response?.type+"");
-        if (response?.type === "success") {
-          // setToken(response.authentication.accessToken);
-          //getUserInfo(response.authentication.accessToken);
-        }
-      } else {
-        setUserInfo(user);
-        console.log("loaded locally");
-      }
-    }
-
-    const getLocalUser = async () => {
-      const data = await AsyncStorage.getItem("@user");
-      if (!data) return null;
-      return JSON.parse(data);
-    };
+    };*/
   
     const handleLogout = async () => {
       await AsyncStorage.removeItem("@user");
@@ -147,15 +156,19 @@ export default function Login({ navigation }: any) {
         }}
       />
         <Button
-        disabled={!request}
-        title="Login"
-        onPress={() => {
-          promptAsync();
-        }}
-      />
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+        />
       <Button title="logout" onPress={() => handleLogout()} />
 
-
+      <TextInput
+        placeholder="login test"
+        onChangeText={(text) => setValue("socialId", text)}
+      />
+      <Button title="Login Test" onPress={handleSubmit(handleLoginTest)} />
 
       <Text>{JSON.stringify(userInfo, null, 2)}</Text>
     </View>
