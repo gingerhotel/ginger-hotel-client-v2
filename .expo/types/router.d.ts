e873fa53675e6d5f0ce0350f95464a88c0289666
minsu.lee <minsu.rob@gmail.com>
2023-11-23 17:29:43 +0900

    etc: 미사용 파일 삭제

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -7,7 +7,7 @@ declare module "expo-router" {
   export * from 'expo-router/build';
 
   // prettier-ignore
-  type StaticRoutes = `/` | `/(tabs)/_layout` | `/_layout` | `/(tabs)/hotels` | `/hotels` | `/(tabs)/my` | `/my` | `/(tabs)/village` | `/village` | `/(tabs) copy/_layout` | ` copy/_layout` | `/answer` | `/create` | `/createHotelAgree` | `/createHotelName` | `/createHotelSelect` | `/feekCharge` | `/gingerAlbum` | `/gingercard` | `/guidePage` | `/instaShared` | `/letter` | `/letterCompleted` | `/login` | `/mailbox` | `/modal` | `/navigation/` | `/push` | `/tabs/hotels` | `/tabs/my` | `/tabs/village`;
+  type StaticRoutes = `/` | `/(tabs)/_layout` | `/_layout` | `/(tabs)/hotels` | `/hotels` | `/(tabs)/my` | `/my` | `/(tabs)/village` | `/village` | `/(tabs) copy/_layout` | ` copy/_layout` | `/answer` | `/create` | `/createHotelAgree` | `/createHotelName` | `/createHotelSelect` | `/feekCharge` | `/gingerAlbum` | `/gingercard` | `/guidePage` | `/instaShared` | `/letter` | `/letterCompleted` | `/login` | `/mailbox` | `/modal` | `/navigation/` | `/push`;
   // prettier-ignore
   type DynamicRoutes<T extends string> = `/${CatchAllRoutePart<T>}`;
   // prettier-ignore
