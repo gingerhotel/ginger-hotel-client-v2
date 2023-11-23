09160d27e93a0ad4641c745e73bb51ec3853aad4
2ujin <suddenlydisappear200@gmail.com>
2023-11-23 21:57:26 +0000

    feat: 폰트 로드 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,36 +4,53 @@ import { Stack, useRouter } from 'expo-router'
 import * as Font from 'expo-font';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { useFonts } from "expo-font";
 
 export default function _layout() {
-    const router = useRouter();
-    const queryClient = new QueryClient();
-    return (
-        <QueryClientProvider client={queryClient}>
-            <RecoilRoot>
-                <Stack
-                    screenOptions={{
-                        // headerStyle: {
-                        //     backgroundColor: 'black'
-                        // },
-                        // headerTintColor: 'white'
-                    }}
-                >
-                        <Stack.Screen name="index" options={{
-                            title: 'Home',
-                            headerShown: false,
-                        }} />
-                        <Stack.Screen name="(tabs)" options={{
-                            headerShown: false,
-                        }} />
-                        <Stack.Screen name="[missing]" options={{
-                            title: '404'
-                        }} />
+  const queryClient = new QueryClient();
+  const [fontsLoaded] = useFonts({
+    "NanumSquareNeo-Variable": require("../assets/fonts/NanumSquareNeo-Variable.ttf"),
+    "SOYOMaple-Regular": require("../assets/fonts/SOYO-Maple-Regular.ttf"),
+    "Quicksand-Variable": require("../assets/fonts/Quicksand-VariableFont_wght.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <></>;
+  }
 
-                </Stack>
-        </RecoilRoot>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Stack
+          screenOptions={
+            {
+              // headerStyle: {
+              //     backgroundColor: 'black'
+              // },
+              // headerTintColor: 'white'
+            }
+          }
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Home",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="[missing]"
+            options={{
+              title: "404",
+            }}
+          />
+        </Stack>
+      </RecoilRoot>
     </QueryClientProvider>
-    )
-
-  
+  );
 }
