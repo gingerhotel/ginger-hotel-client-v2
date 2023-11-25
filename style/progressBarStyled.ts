cd20f52ee753a6380babdc91ba2a1847838b94e6
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-25 13:12:58 +0900

    feat: 프로그래스바 기능 구현
    디자인은 아직 미완 텍스트부분

--- style/progressBarStyled.ts
+++ style/progressBarStyled.ts
@@ -9,7 +9,21 @@ export const ProgressBarView = styled.View`
   width: 60%;
   margin-top: 25px;
 `;
+export const BarView = styled.View`
+  flex: 1;
+  justify-content: center;
+  flex-direction: row;
+  background-color: #36363B;
+  border: none;
+  border-radius: 10px;
+`;
 export const Bar = styled.View`
   flex: 1;
   justify-content: center;
+`;
+
+export const BarViewText = styled.View`
+`;
+export const BarText = styled.Text`
+
 `;
