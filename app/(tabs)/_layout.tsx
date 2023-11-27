d75af58d263ff1b51a5824657b5f625bdced85b1
minsu.lee <minsu.rob@gmail.com>
2023-11-27 19:25:27 +0900

    fix: 하단 Tab 디자인 원복

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router/tabs'
+import { TabBarIcon } from '../tab/TabBarIcon';
 
 export default function _layout() {
   return (
@@ -12,20 +13,22 @@ export default function _layout() {
         options={{
           headerShown: false,
           title: "빌리지",
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="hotel/[id]"
         options={{
           title: "진저호텔",
+          tabBarIcon: ({ color }) => <TabBarIcon name="hotel" color={color} />,
         }}
-        
       />
       <Tabs.Screen
         name="my"
         options={{
           headerShown: false,
           title: "마이페이지",
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
     </Tabs>
