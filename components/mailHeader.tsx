968d8cb67893db69301340bac5b4e41996875154
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-28 04:51:48 +0900

    feat: 현재 유저가 가지고 있는 편지함 수를 보여주도록 기능 구현

--- components/mailHeader.tsx
+++ components/mailHeader.tsx
@@ -16,14 +16,15 @@ import {
   MailTitleText,
   MailTitleView
 } from "../style/mailBoxStyled";
-import { useRecoilState } from "recoil";
-import { letterSwitchState } from "../atom/letterAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { letterSwitchState, newLetterCountState } from "../atom/letterAtom";
 import { getCurrentDateDot } from "../data/data";
 
 const arrow = require("../assets/icon/i_left_arrow.svg")
 const MailHeader = ({ marginTop, isTitle = true, navigation }: any) => {
   const [letterCheck, setLetterCheck] = useRecoilState(letterSwitchState);
   const [date, setDate] = useState<string>("");
+  const newLetterCount = useRecoilValue(newLetterCountState);
   useEffect(() => {
     setDate(getCurrentDateDot())
   }, [])
@@ -64,7 +65,7 @@ const MailHeader = ({ marginTop, isTitle = true, navigation }: any) => {
         <MailInfoView>
           <TouchableOpacity onPress={() => onLetterChange('1')}>
             <MailChoseView b_color={letterCheck.new ? "#34AB96" : "#000"} >
-              <MailChoseText f_color={letterCheck.new ? "#fff" : "#6E6E73"}>새로운 편지 2</MailChoseText>
+              <MailChoseText f_color={letterCheck.new ? "#fff" : "#6E6E73"}>새로운 편지 {newLetterCount}</MailChoseText>
             </MailChoseView>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => onLetterChange('2')}>
