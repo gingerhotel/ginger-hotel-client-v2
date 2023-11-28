634c8d35650e72047aae44cb9fc683359ebc250c
minsuRob <minsu.rob@gmail.com>
2023-11-28 15:32:42 +0900

    fix: 테스트페이지 수정

--- app/testdev.tsx
+++ app/testdev.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 import { View, useWindowDimensions, StyleSheet, Text } from "react-native";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import { getHotel } from "../api/hotelApi";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { HOTEL_URL } from "../api/url";
 
 export default function testDev() {
   
+      const getHotel = async (id: string) => {
+    try {
+      const accessToken = await AsyncStorage.getItem("accessToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+      const response = await axios.get(`${HOTEL_URL}/${id}`);
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  };
+
+  React.useEffect(()=>{
+    const data = getHotel("1");
+  }, 
+  [])
 
   return (
     <>
