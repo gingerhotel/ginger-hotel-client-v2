df8dad6ff203cf8b4cd8c73d9165626daae14524
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-29 01:07:16 +0900

    feat: 삭제 차단 api 추가

--- api/letterApi.ts
+++ api/letterApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LETTERS_URL } from "./url";
+import { LETTERS_HOTEL_URL, LETTERS_URL } from "./url";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getCurrentDate } from "../data/data";
 import { NewLetter } from "./interface";
@@ -8,23 +8,43 @@ import { axiosConfig } from "./commonApi";
 axiosConfig();
 
 export const newLetterData = async (props: any) => {
-    const date = getCurrentDate()
-    try {
-        const response = await axios.get(`${LETTERS_URL}/${props.hotelId}?date=${date}`);
-        return response.data;
-    } catch (e) {
-        console.error(e);
-        //throw e;
-        return true; // Todo: Need to validate
-    }
+  const date = getCurrentDate()
+  try {
+    const response = await axios.get(`${LETTERS_HOTEL_URL}/${props.hotelId}?date=${date}`);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    //throw e;
+    return true; // Todo: Need to validate
+  }
 };
 
 export const newLetter = async (props: NewLetter) => {
-    try {
-      const response = await axios.post(`${LETTERS_URL}/${props.hotelId}`, props);
-      return response.data;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  };
+  try {
+    const response = await axios.post(`${LETTERS_HOTEL_URL}/${props.hotelId}`, props);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+export const letterDelete = async (letterId: number) => {
+  try {
+    const response = await axios.delete(`${LETTERS_URL}/${letterId}`);
+    console.error(response)
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+export const letterBlock = async (letterId: number) => {
+  try {
+    const response = await axios.post(`${LETTERS_URL}/${letterId}/block`);
+    console.error(response)
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
