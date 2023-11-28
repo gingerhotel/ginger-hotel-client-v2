976c882fb4a556f839556e2bd90cdba7cc35b12e
minsuRob <minsu.rob@gmail.com>
2023-11-29 00:21:15 +0900

    fix: 편지 열람 조건 추가

--- app/(tabs)/hotel/[id].tsx
+++ app/(tabs)/hotel/[id].tsx
@@ -139,7 +139,7 @@ export default function HotelComp() {
                   color="green"
                   width={288}
                   url="mailbox"
-                  is_disable={!data?.canReceiveLetterToday}
+                  is_disable={data?.canReceiveLetterToday}
                 />
                 <TouchableOpacity>
                   <SvgImg
