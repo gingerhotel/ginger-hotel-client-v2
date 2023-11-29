28d3bc83bc4e9e99b2497f3e2da0f8a3b93b1569
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-29 20:28:19 +0900

    feat: 차단을 하였을 때 실시간으로 바뀌도록 변경

--- components/Modal/blockModal.tsx
+++ components/Modal/blockModal.tsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import { DeleteModaContentlView, DeleteModalButtonView, DeleteModalView } from '../../style/deleteModalStyled'
 import { Text, View } from '../themed'
 import Modal from 'react-native-modal';
-import { BottomSheetDeleteProps } from '../../api/interface';
+import { BottomSheetProps } from '../../api/interface';
 import { MonoText } from '../styledText';
 import { SvgImg } from '../svgImg';
 import { letterBlock, letterDelete } from '../../api/letterApi';
 import { useMutation, useQueryClient } from 'react-query';
+import { useRecoilState } from 'recoil';
+import { letterUpdateState } from '../../atom/letterAtom';
 const i_yes = require("../../assets/icon/i_block_yes.svg");
 const i_no = require("../../assets/icon/i_no.svg");
-const BlockModal = ({ isVisible, onClose, letterId }: BottomSheetDeleteProps) => {
-    const deleteMutation = useMutation((letterId: any) => letterBlock(letterId));
-    const onDelelet = async () => {
-        deleteMutation.mutate(letterId)
+const BlockModal = ({ isVisible, onClose, letterId }: BottomSheetProps) => {
+    const [blockCheck, setBlockCheck] = useRecoilState(letterUpdateState);
+    const onBlock = async () => {
+        await letterBlock(letterId);
+        setBlockCheck(!blockCheck);
         onClose();
     }
     return (
@@ -24,10 +27,10 @@ const BlockModal = ({ isVisible, onClose, letterId }: BottomSheetDeleteProps) =>
             <DeleteModalView>
                 <DeleteModaContentlView>
                     <MonoText style={{ fontSize: 16, fontWeight: 'bold' }}>사용자를 차단하시겠습니까?</MonoText>
-                    <MonoText style={{ color: '#98989B' }}>한번 삭제한 편지는 복구가 불가해요.</MonoText>
+                    <MonoText style={{ color: '#98989B' }}>차단한 사용자로부터는 편지를 받을 수 없어요.</MonoText>
                     <DeleteModalButtonView>
                         <SvgImg url={i_no} width={141} height={46} onPress={onClose} />
-                        <SvgImg url={i_yes} width={141} height={46} onPress={onDelelet} />
+                        <SvgImg url={i_yes} width={141} height={46} onPress={onBlock} />
                     </DeleteModalButtonView>
                 </DeleteModaContentlView>
             </DeleteModalView>
