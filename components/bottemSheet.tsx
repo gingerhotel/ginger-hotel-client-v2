0edb7ae6a709247ebb776327cc10456c34b957c4
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-29 20:25:55 +0900

    feat: 차단 해제 모달 추가

--- components/bottemSheet.tsx
+++ components/bottemSheet.tsx
@@ -9,7 +9,7 @@ import { MonoText } from './styledText';
 import DeleteModal from './Modal/deleteModal';
 import { BottomSheetDeleteProps, BottomSheetProps } from '../api/interface';
 import BlockModal from './Modal/blockModal';
-
+import UnBlockModal from './Modal/unBlockModal';
 
 const i_drag = require("../assets/icon/i_drag_handle.svg");
 const i_incognito = require("../assets/icon/i_incognito.svg");
@@ -17,19 +17,24 @@ const i_peek = require("../assets/icon/i_peek.svg");
 const i_reply_plus = require("../assets/icon/i_reply_plus.svg");
 const i_delete = require("../assets/icon/i_delete.svg");
 const i_pro = require("../assets/icon/i_pro.svg");
-const BottomSheet = ({ isVisible, onClose, letterId }: BottomSheetDeleteProps) => {
+const BottomSheet = ({ isVisible, onClose, letterId, blocked }: BottomSheetDeleteProps) => {
     const [letterDelete, setLetterDelete] = useState(false)
     const [letterBlock, setLetterBlock] = useState(false)
+    const [letterUnBlock, setLetterUnBlock] = useState(false)
     const handleSwipeMove = (gestureState: any) => {
         console.log(gestureState.dy);
     };
     const handleDelete = () => {
-        setLetterDelete(true)
-        onClose()
+        setLetterDelete(true);
+        onClose();
     }
     const handleBlock = () => {
-        setLetterBlock(true)
-        onClose()
+        setLetterBlock(true);
+        onClose();
+    }
+    const handleUnBlock = () => {
+        setLetterBlock(true);
+        onClose();
     }
     const closeModal = () => {
         setLetterDelete(false)
@@ -53,30 +58,41 @@ const BottomSheet = ({ isVisible, onClose, letterId }: BottomSheetDeleteProps) =
                             <SvgImg url={i_drag} width={32} height={4} onPress={onClose} />
                         </BottemSheetDraeView>
                         <BottemSheetBorderView>
-                            <BottemSheetElementView>
-                                <SvgImg url={i_reply_plus} width={32} height={32}></SvgImg>
-                                <MonoText>답장하기</MonoText>
-                                <SvgImg url={i_pro} width={32} height={32}></SvgImg>
-                            </BottemSheetElementView>
-                            <BottemSheetElementView>
-                                <SvgImg url={i_peek} width={32} height={32}></SvgImg>
-                                <MonoText>엿보기</MonoText>
-                            </BottemSheetElementView>
+                            {blocked ? (null) : (
+                                <>
+                                    <BottemSheetElementView>
+                                        <SvgImg url={i_reply_plus} width={32} height={32}></SvgImg>
+                                        <MonoText>답장하기</MonoText>
+                                        <SvgImg url={i_pro} width={32} height={32}></SvgImg>
+                                    </BottemSheetElementView>
+                                    <BottemSheetElementView>
+                                        <SvgImg url={i_peek} width={32} height={32}></SvgImg>
+                                        <MonoText>엿보기</MonoText>
+                                    </BottemSheetElementView>
+                                </>
+                            )}
                             <BottemSheetElementView onPress={handleDelete}>
                                 <SvgImg url={i_delete} width={32} height={32}></SvgImg>
                                 <MonoText>편지 삭제하기</MonoText>
                             </BottemSheetElementView>
                         </BottemSheetBorderView>
-                        <BottemSheetElementView onPress={handleBlock}>
-                            <SvgImg url={i_incognito} ></SvgImg>
-                            <MonoText>사용자 차단하기</MonoText>
-                        </BottemSheetElementView>
+                        {blocked ? (
+                            <BottemSheetElementView onPress={handleBlock}>
+                                <SvgImg url={i_incognito} ></SvgImg>
+                                <MonoText>사용자 차단 해제하기</MonoText>
+                            </BottemSheetElementView>
+                        ) : (
+                            <BottemSheetElementView onPress={handleUnBlock}>
+                                <SvgImg url={i_incognito} ></SvgImg>
+                                <MonoText>사용자 차단하기</MonoText>
+                            </BottemSheetElementView>
+                        )}
                     </BottemSheetContentView>
-                </BottemSheetView>
-            </Modal>
+                </BottemSheetView >
+            </Modal >
             <DeleteModal isVisible={letterDelete} onClose={closeModal} letterId={letterId} />
             <BlockModal isVisible={letterBlock} onClose={closeModal} letterId={letterId} />
-
+            <UnBlockModal isVisible={letterBlock} onClose={closeModal} letterId={letterId} />
         </>
     );
 };
