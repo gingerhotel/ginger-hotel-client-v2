86d5f102d3155fa79b4bb0fc64425730cb2d91b8
seocylucky <chaeyeon2285@naver.com>
2023-11-29 16:25:50 +0900

    feat: 하단바 아이콘 커스텀 작업

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router/tabs'
-import { TabBarIcon } from '../tab/TabBarIcon';
-import { colors } from '../../constants/Colors';
+import { Tabs } from "expo-router/tabs";
+import { TabBarIcon } from "../tab/TabBarIcon";
+import { colors } from "../../constants/Colors";
 
 export default function _layout() {
   return (
     <Tabs
-      initialRouteName='hotel/[id]'
+      initialRouteName="hotel/[id]"
       screenOptions={{
         tabBarInactiveTintColor: "gray",
-        tabBarStyle : {
+        tabBarActiveTintColor: colors.Whiteyello,
+        tabBarStyle: {
           backgroundColor: colors.greyblack,
           borderTopWidth: 0,
           marginBottom: 0,
-        }
+          flexDirection: "column",
+        },
+        tabBarLabelStyle: {
+          fontSize: 10, // 텍스트 크기 조절
+        },
       }}
-      >
+    >
       <Tabs.Screen
         name="village"
         options={{
           headerShown: false,
           title: "앨범",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="album" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="hotel/[id]"
         options={{
           title: "진저호텔",
-          tabBarIcon: ({ color }) => <TabBarIcon name="hotel" color={color} />,
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="hotel" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="my"
         options={{
           headerShown: false,
           title: "마이페이지",
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="user" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
