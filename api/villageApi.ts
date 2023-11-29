f6c28b6995a800beb86173dd065a6f1f8a745442
2ujin <suddenlydisappear200@gmail.com>
2023-11-29 22:15:37 +0000

    feat: 내 빌리지 추가하기

---
+++ api/villageApi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { LETTERS_HOTEL_URL, LETTERS_URL, VILLAGE_URL } from "./url";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getCurrentDate } from "../data/data";
+import { NewLetter } from "./interface";
+
+export const addVillage = async (hotelId: string) => {
+  try {
+    const accessToken = await AsyncStorage.getItem("accessToken");
+    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    const response = await axios.post(`${VILLAGE_URL}/hotel/${hotelId}`);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
