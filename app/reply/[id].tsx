0ebf459aa14654dd88502a4ae3dcacc8ce58920a
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-30 21:31:03 +0900

    feat: [id].tsx 수정
    시간이 없어 빠르게 올리는 점 죄송합니다.

--- app/reply/[id].tsx
+++ app/reply/[id].tsx
@@ -5,17 +5,16 @@ import Buttons from "../../components/buttons";
 import { MonoText } from "../../components/styledText";
 import { colors } from "../../constants/Colors";
 import { useMutation } from "react-query";
-import { newLetter } from "../../api/letterApi";
 
 import styled from "styled-components/native";
 import { router, useLocalSearchParams } from "expo-router";
 import ErrorModal from "../../components/Modal/errorModal";
 import { ErrorMessageConverter } from "../../data/error-message-converter";
 import { useNavigation } from "expo-router";
-import { useRoute } from "@react-navigation/native";
 import { useRecoilValue } from "recoil";
 import { replyNameState } from "../../atom/letterAtom";
-import ReplyBoxHeader from "../../components/replyBoxHeader";
+import ReplyHeader from "../../components/replyHeader";
+import { newReply } from "../../api/repliesApi";
 
 export default function Reply() {
     const navigation = useNavigation();
@@ -43,7 +42,7 @@ export default function Reply() {
     };
 
     const mutation = useMutation(
-        newLetter, // 이 함수가 서버로 데이터를 전송하는 역할을 합니다.
+        newReply, // 이 함수가 서버로 데이터를 전송하는 역할을 합니다.
         {
             onSuccess: (data) => {
                 router.push("/letterCompleted") // 성공한 경우에 response 데이터를 사용할 수 있습니다.
@@ -55,9 +54,8 @@ export default function Reply() {
         try {
             const letterData = {
                 content: data.letters,
-                senderNickname: data.nickname,
                 image: "",
-                hotelId: id.toString()
+                letterId: id.toString()
             };
             // 뮤테이션 실행
             await mutation.mutateAsync(letterData);
@@ -74,10 +72,10 @@ export default function Reply() {
 
     return (
         <View style={styles.container}>
-            <ReplyBoxHeader />
+            <ReplyHeader />
             <View style={styles.mailbox_items}>
                 <MonoText style={{ color: colors.Whiteyello, marginBottom: 12 }}>
-                    따뜻한 편지를 보내 준 친구에게 마음을 전해요{" "}
+                    따뜻한 편지를 보내 준 친구에게 마음을 전해요
                 </MonoText>
 
                 <TextInput
@@ -90,6 +88,7 @@ export default function Reply() {
                         setValue("letters", text)
                     }}
                     maxLength={300}
+                    placeholderTextColor={colors.grey500}
                 />
                 <View style={styles.nickname_input}>
                     <MonoText style={styles.input_text}>받는 이</MonoText>
@@ -106,7 +105,6 @@ export default function Reply() {
           color="darkgray"
         />
         */}
-                <MonoText>{'   '}</MonoText>
                 <Buttons
                     url={"letterCompleted"}
                     title="보내기"
@@ -114,6 +112,7 @@ export default function Reply() {
                     color="green"
                     callback={handleSubmit(letterSubmit)}
                     is_disable={!isNotEmptyLetters}
+                    width={330}
                 />
             </View>
             <ErrorModal
@@ -132,32 +131,30 @@ export default function Reply() {
 const styles = StyleSheet.create({
     container: {
         alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-        backgroundColor: "#000",
+        justifyContent: "space-between",
+        gap: 50,
+        backgroundColor: colors.greyblack,
+        paddingBottom: 60,
+        height: '100%'
     },
     mailbox_items: {
-        padding: 15,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
-        height: "100%",
-        flex: 1,
     },
     nickname_input: {
         flexDirection: "row",
         alignItems: "center",
         width: 300,
         backgroundColor: colors.grey900,
-        paddingTop: 12,
-        paddingBottom: 12,
+        paddingTop: 20,
+        paddingBottom: 20,
         paddingLeft: 20,
         borderRadius: 6,
         marginTop: 20,
         textAlign: "left",
         marginBottom: 20,
-        justifyContent: 'space-between'
     },
     input_text: {
         fontSize: 12,
@@ -176,67 +173,33 @@ const styles = StyleSheet.create({
         outlineStyle: "none",
     },
     footer: {
-        width: "100%",
         display: "flex",
-        justifyContent: "space-between",
-        flexDirection: "row",
+        flexDirection: 'row',
+        justifyContent: "center",
         alignItems: "center",
-        paddingBottom: 60,
-        paddingLeft: 20,
-        paddingRight: 20,
-        border: "none",
-        marginTop: -3,
-
+        backgroundColor: colors.greyblack,
     },
     letter: {
-        width: 300,
-        height: 400,
+        width: 287,
+        height: 326,
         backgroundColor: colors.grey900,
         paddingTop: 22,
         paddingBottom: 22,
         paddingLeft: 18,
         paddingRight: 18,
         textAlign: "left",
-        border: "4px solid #719898",
+        border: "5px dashed #005142",
         borderRadius: 12,
-        color: colors.grey500,
+        color: '#FFFDF0',
         outlineStyle: "none",
         fontFamily: "NanumSquareNeo-Variable",
         lineHeight: 18,
     },
+    letter_box: {
+        backgroundColor: colors.grey900,
+        padding: 7,
+        border: "3px solid  #005142",
+        borderRadius: 18,
+        lineHeight: 18,
+    },
 });
-
-export const LetterOuterContainer = styled.View`
-    border-radius: 18px;
-    border-width: 2px;
-    border-color: #25796B;
-    width:384px;
-    padding: 7px;
-`
-
-// Styled component with linear gradient
-export const LetterInnerContainer = styled.View<{ b_color?: string }>`
-  border-radius: 12px;
-  border-width: 4px;
-  border-color: #005142;
-  background-color: ${(props) => props.b_color};
-  border-style: dotted;
-`;
-export const LetterInnerInfoView = styled.View`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-`
-
-export const LetterInnerTitieView = styled.View`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    max-width: 310px;
-    min-width: 310px;
-    border-bottom-width: 1px;
-    padding: 10px;
-`
-
