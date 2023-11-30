8792d14ef7cbdfbb0bce414da3669a3ec2d7d864
seocylucky <chaeyeon2285@naver.com>
2023-12-01 06:13:35 +0900

    fix: 광고 추가

--- app/letterCompleted.tsx
+++ app/letterCompleted.tsx
@@ -15,6 +15,7 @@ import { colors } from "../constants/Colors";
 import { SvgImg } from "../components/svgImg";
 import { useRecoilState } from "recoil";
 import { hotelIdState } from "../atom/letterAtom";
+import KakaoAdFit from "../advertisement/KakaoAdFit";
 const i_reply_letter = require("../assets/images/i_reply_letter.svg");
 export default function LetterCompleted() {
   const navigation = useNavigation();
@@ -22,13 +23,13 @@ export default function LetterCompleted() {
     navigation.setOptions({ headerShown: false });
   }, [navigation]);
 
-
   const routes = navigation.getState()?.routes;
   const prevRoute = routes[routes.length - 2];
   const isAnswer = prevRoute.name === "answer" ? true : false;
   const [hotelId, setHotelId] = useRecoilState<string | string[]>(hotelIdState);
   return (
     <View style={styles.container}>
+      <KakaoAdFit />
       <View style={styles.mailbox_items}>
         <MonoText style={styles.title}>
           {isAnswer ? "답장을" : "편지를"} 보냈어요!
@@ -36,9 +37,7 @@ export default function LetterCompleted() {
         <MonoText style={styles.text}>
           당신의 소중한 마음을 온전히 전달해 드릴게요 :)
         </MonoText>
-        <SvgImg
-          url={i_reply_letter}
-        />
+        <SvgImg url={i_reply_letter} />
       </View>
 
       <View style={styles.footer}>
@@ -76,7 +75,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     flex: 1,
-    gap: 30
+    gap: 30,
   },
   footer: {
     width: "100%",
