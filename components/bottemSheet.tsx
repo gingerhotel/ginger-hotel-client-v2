c5b731049afb97b6469fee35350d6d7bc52ff141
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-11-30 15:33:26 +0000

    feat: 답장쪽 qa 수정 날짜 오류 수정

--- components/bottemSheet.tsx
+++ components/bottemSheet.tsx
@@ -11,6 +11,7 @@ import { BottomSheetDeleteProps, BottomSheetProps } from '../api/interface';
 import BlockModal from './Modal/blockModal';
 import UnBlockModal from './Modal/unBlockModal';
 import ReplyModal from './Modal/replyModal';
+import { colors } from "../constants/Colors";
 
 const i_drag = require("../assets/icon/i_drag_handle.svg");
 const i_incognito = require("../assets/icon/i_incognito.svg");
@@ -18,91 +19,126 @@ const i_peek = require("../assets/icon/i_peek.svg");
 const i_reply_plus = require("../assets/icon/i_reply_plus.svg");
 const i_delete = require("../assets/icon/i_delete.svg");
 const i_pro = require("../assets/icon/i_pro.svg");
-const BottomSheet = ({ isVisible, onClose, letterId, blocked }: BottomSheetDeleteProps) => {
-    const [letterDelete, setLetterDelete] = useState(false)
-    const [letterBlock, setLetterBlock] = useState(false)
-    const [letterUnBlock, setLetterUnBlock] = useState(false)
-    const [letterReply, setLetterReply] = useState(false)
-    const handleSwipeMove = (gestureState: any) => {
-        console.log(gestureState.dy);
-    };
-    const handleDelete = () => {
-        setLetterDelete(true);
-        onClose();
-    }
-    const handleBlock = () => {
-        setLetterBlock(true);
-        onClose();
-    }
-    const handleUnBlock = () => {
-        setLetterUnBlock(true);
-        onClose();
-    }
-    const handleReply = () => {
-        setLetterReply(true);
-        onClose();
-    }
-    const closeModal = () => {
-        setLetterDelete(false);
-        setLetterBlock(false);
-        setLetterUnBlock(false);
-        setLetterReply(false);
-    }
-    console.log(letterId);
-    return (
-        <>
-            <Modal
-                isVisible={isVisible}
-                onSwipeComplete={onClose}
-                swipeDirection={['down', 'up']}
-                onSwipeMove={handleSwipeMove}
-                onSwipeStart={() => console.log('Swipe started')}
-                style={{ margin: 0, backgroundColor: 'transparent' }}
-                backdropOpacity={0.2}
-            >
-                <BottemSheetView>
-                    <BottemSheetContentView>
-                        <BottemSheetDraeView >
-                            <SvgImg url={i_drag} width={32} height={4} onPress={onClose} />
-                        </BottemSheetDraeView>
-                        <BottemSheetBorderView>
-                            {blocked ? (null) : (
-                                <>
-                                    <BottemSheetElementView onPress={handleReply}>
-                                        <SvgImg url={i_reply_plus} width={32} height={32}></SvgImg>
-                                        <MonoText>답장하기</MonoText>
-                                    </BottemSheetElementView>
-                                    {/* <BottemSheetElementView>
+const BottomSheet = ({
+  isVisible,
+  onClose,
+  letterId,
+  blocked,
+}: BottomSheetDeleteProps) => {
+  const [letterDelete, setLetterDelete] = useState(false);
+  const [letterBlock, setLetterBlock] = useState(false);
+  const [letterUnBlock, setLetterUnBlock] = useState(false);
+  const [letterReply, setLetterReply] = useState(false);
+  const handleSwipeMove = (gestureState: any) => {
+    console.log(gestureState.dy);
+  };
+  const handleDelete = () => {
+    setLetterDelete(true);
+    onClose();
+  };
+  const handleBlock = () => {
+    setLetterBlock(true);
+    onClose();
+  };
+  const handleUnBlock = () => {
+    setLetterUnBlock(true);
+    onClose();
+  };
+  const handleReply = () => {
+    setLetterReply(true);
+    onClose();
+  };
+  const closeModal = () => {
+    setLetterDelete(false);
+    setLetterBlock(false);
+    setLetterUnBlock(false);
+    setLetterReply(false);
+  };
+
+  const onBackButtonPress = () => {
+    onClose();
+  };
+  console.log(letterId);
+  return (
+    <>
+      <Modal
+        isVisible={isVisible}
+        onBackButtonPress={onBackButtonPress}
+        onBackdropPress={onBackButtonPress}
+        onSwipeComplete={onClose}
+        swipeDirection={["down", "up"]}
+        onSwipeMove={handleSwipeMove}
+        onSwipeStart={() => console.log("Swipe started")}
+        style={{ margin: 0, backgroundColor: "transparent" }}
+        backdropOpacity={0.2}
+      >
+        <BottemSheetView>
+          <BottemSheetContentView>
+            <BottemSheetDraeView>
+              <SvgImg url={i_drag} width={32} height={4} onPress={onClose} />
+            </BottemSheetDraeView>
+            <BottemSheetBorderView>
+              {blocked ? null : (
+                <>
+                  <BottemSheetElementView onPress={handleReply}>
+                    <SvgImg url={i_reply_plus} width={32} height={32}></SvgImg>
+                    <MonoText style={{ color: colors.Whiteyello }}>
+                      답장하기
+                    </MonoText>
+                  </BottemSheetElementView>
+                  {/* <BottemSheetElementView>
                                         <SvgImg url={i_peek} width={32} height={32}></SvgImg>
                                         <MonoText>엿보기</MonoText>
                                     </BottemSheetElementView> */}
-                                </>
-                            )}
-                            <BottemSheetElementView onPress={handleDelete}>
-                                <SvgImg url={i_delete} width={32} height={32}></SvgImg>
-                                <MonoText>편지 삭제하기</MonoText>
-                            </BottemSheetElementView>
-                        </BottemSheetBorderView>
-                        {!blocked ? (
-                            <BottemSheetElementView onPress={handleBlock}>
-                                <SvgImg url={i_incognito} ></SvgImg>
-                                <MonoText>사용자 차단 하기</MonoText>
-                            </BottemSheetElementView>
-                        ) : (
-                            <BottemSheetElementView onPress={handleUnBlock}>
-                                <SvgImg url={i_incognito} ></SvgImg>
-                                <MonoText>사용자 차단 해제하기</MonoText>
-                            </BottemSheetElementView>
-                        )}
-                    </BottemSheetContentView>
-                </BottemSheetView >
-            </Modal >
-            <DeleteModal isVisible={letterDelete} onClose={closeModal} letterId={letterId} />
-            <BlockModal isVisible={letterBlock} onClose={closeModal} letterId={letterId} />
-            <UnBlockModal isVisible={letterUnBlock} onClose={closeModal} letterId={letterId} />
-            <ReplyModal isVisible={letterReply} onClose={closeModal} letterId={letterId} />
-        </>
-    );
+                </>
+              )}
+              <BottemSheetElementView onPress={handleDelete}>
+                <SvgImg url={i_delete} width={32} height={32}></SvgImg>
+                <MonoText style={{ color: colors.Whiteyello }}>
+                  편지 삭제하기
+                </MonoText>
+              </BottemSheetElementView>
+            </BottemSheetBorderView>
+            {!blocked ? (
+              <BottemSheetElementView onPress={handleBlock}>
+                <SvgImg url={i_incognito}></SvgImg>
+                <MonoText style={{ color: colors.Whiteyello }}>
+                  사용자 차단 하기
+                </MonoText>
+              </BottemSheetElementView>
+            ) : (
+              <BottemSheetElementView onPress={handleUnBlock}>
+                <SvgImg url={i_incognito}></SvgImg>
+                <MonoText style={{ color: colors.Whiteyello }}>
+                  사용자 차단 해제하기
+                </MonoText>
+              </BottemSheetElementView>
+            )}
+          </BottemSheetContentView>
+        </BottemSheetView>
+      </Modal>
+      <DeleteModal
+        isVisible={letterDelete}
+        onClose={closeModal}
+        letterId={letterId}
+      />
+      <BlockModal
+        isVisible={letterBlock}
+        onClose={closeModal}
+        letterId={letterId}
+      />
+      <UnBlockModal
+        isVisible={letterUnBlock}
+        onClose={closeModal}
+        letterId={letterId}
+      />
+      <ReplyModal
+        isVisible={letterReply}
+        onClose={closeModal}
+        letterId={letterId}
+      />
+    </>
+  );
 };
 
 export default BottomSheet;
