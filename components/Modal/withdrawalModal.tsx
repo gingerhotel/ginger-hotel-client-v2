616723e2a3246eb8cafa204d4bc1f1d832474508
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2023-11-30 23:27:08 +0900

    feat: 회원탈퇴인 경우 나타나는 모달 추가

---
+++ components/Modal/withdrawalModal.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+    DeleteModaContentlView,
+    DeleteModalButtonView,
+    DeleteModalView,
+} from "../../style/deleteModalStyled";
+import { Text, View } from "../themed";
+import Modal from "react-native-modal";
+import { BottomSheetProps } from "../../api/interface";
+import { MonoText } from "../styledText";
+import { SvgImg } from "../svgImg";
+import { letterDelete } from "../../api/letterApi";
+import { useMutation, useQueryClient } from "react-query";
+import { useRecoilState } from "recoil";
+import { letterUpdateState } from "../../atom/letterAtom";
+import Buttons from "../buttons";
+const WithdrawalModal = ({
+    isVisible,
+    onClose,
+    letterId,
+}: BottomSheetProps) => {
+    return (
+        <Modal
+            isVisible={isVisible}
+            style={{ margin: 0, backgroundColor: "transparent" }}
+            backdropOpacity={0.2}
+        >
+            <DeleteModalView>
+                <DeleteModaContentlView>
+                    <MonoText style={{ fontSize: 16, fontWeight: "bold" }}>
+                        답장을 보낼 수 없어요!
+                    </MonoText>
+                    <MonoText style={{ color: "#98989B" }}>
+                        탈퇴한 회원이에요.
+                    </MonoText>
+                    <DeleteModalButtonView>
+                        <Buttons
+                            title='확인'
+                            width={141}
+                            color='gray_700'
+                            callback={onClose}
+                            auth={true}
+                        />
+                    </DeleteModalButtonView>
+                </DeleteModaContentlView>
+            </DeleteModalView>
+        </Modal>
+    );
+};
+
+export default WithdrawalModal;
