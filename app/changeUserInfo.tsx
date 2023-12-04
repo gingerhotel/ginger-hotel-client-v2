8220f53463affdd0abaf6a7fc133b926806067bb
minsu.lee <minsu.rob@gmail.com>
2023-12-04 18:14:21 +0900

    fix: date picker 오류로 주석

--- app/changeUserInfo.tsx
+++ app/changeUserInfo.tsx
@@ -158,7 +158,7 @@ const ChangeUserInfo = () => {
               생년월일을 형식에 맞게 입력해주세요.
             </MonoText>
 
-            <View style={styles.input_wrapper}>
+            {/* <View style={styles.input_wrapper}>
               <DatePicker
                 locale={ko}
                 portalId="root-portal"
@@ -169,7 +169,7 @@ const ChangeUserInfo = () => {
                 showYearDropdown
                 popperPlacement="bottom"
               />
-            </View>
+            </View> */}
 
             <TouchableOpacity
               accessible={true}
