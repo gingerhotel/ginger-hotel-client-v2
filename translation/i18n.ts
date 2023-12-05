6bd7eb4c25450932f7ecb962365af596612c8ef8
minsu.lee <44646213+minsuRob@users.noreply.github.com>
2023-12-05 17:07:05 +0900

    feat: i18n.ts 설정 파일 추가 #165

---
+++ translation/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import { getLocales } from "react-native-localize";
+
+import en from "./en.json";
+import ko from "./ko.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ko: {
+    translation: ko,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources: resources,
+  lng: getLocales()[0].languageCode,
+  fallbackLng: "ko",
+  supportedLngs: ["en", "ko"],
+  compatibilityJSON: "v3",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
