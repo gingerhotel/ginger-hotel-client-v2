0ddb54caa2792642317d271063aecf10a90024e7
minsu.lee <minsu.rob@gmail.com>
2023-12-05 23:42:45 +0900

    fix: 인스타 share 라이브러리만 추가

--- components/bottemShareSheet.tsx
+++ components/bottemShareSheet.tsx
@@ -12,6 +12,8 @@ import { colors } from "../constants/Colors";
 import { useRecoilValue } from 'recoil';
 import { FontAwesome } from '@expo/vector-icons';
 
+import Share, {Social} from 'react-native-share';
+
 import * as Clipboard from 'expo-clipboard';
 import Toast from "react-native-toast-message";
 import { push } from 'expo-router/src/global-state/routing';
@@ -44,8 +46,19 @@ const BottemShareSheet = ({
   };
   const goMyHotelCard = () => {
     router.push(`/instaShared/${hotelId}`);
+  // const handleInstaStory = () => {
+  //   Share.shareSingle({
+  //     social: Social.InstagramStories,
+  //     appId: "440384258310203",
+  //     // backgroundImage: "배경으로 지정할 이미지의 URL",
+  //     // backgroundVideo: "배경으로 지정할 동영상의 URL",
+  //     // stickerImage: "sticker 형식으로(작게) 공유할 이미지의 URL", 
+  //     backgroundBottomColor: " #837DF4",
+  //     backgroundTopColor: "#906df4",
+  //   });
     onClose();
   };
+  
   const onBackButtonPress = () => {
     onClose();
   };
@@ -69,6 +82,7 @@ const BottemShareSheet = ({
             </BottemSheetDraeView>
             <BottemSheetBorderView>
             <BottemSheetElementView onPress={goMyHotelCard}>
+            {/* <BottemSheetElementView onPress={handleInstaStory}> */}
               <MonoText style={{ color: colors.Whiteyello, fontSize: 15 }}>
               내 호텔 카드 {/*  <FontAwesome name="instagram" size={16} /> */}
               </MonoText>
