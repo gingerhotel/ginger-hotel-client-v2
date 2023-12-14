ac0d37158f38236329bbde7d52e812ec96c9f795
2ujin <suddenlydisappear200@gmail.com>
2023-12-14 14:40:42 +0000

    feat: 수정

--- app/gingerAlbum.tsx
+++ app/gingerAlbum.tsx
@@ -42,6 +42,8 @@ const bellboy25 = require("../assets/gingerman/Modal_Ginger/modal_drawing.png");
 const bellboy26 = require("../assets/gingerman/Album_Ginger/album_drawing.png");
 const bellboy27 = require("../assets/gingerman/Modal_Ginger/modal_developer.png");
 const bellboy28 = require("../assets/gingerman/Album_Ginger/album_developer.png");
+const bellboy29 = require("../assets/gingerman/Modal_Ginger/modal_science.png");
+const bellboy30 = require("../assets/gingerman/Album_Ginger/album_developer.png");
 
 const GingerAlbum = () => {
   const { id } = useLocalSearchParams();
